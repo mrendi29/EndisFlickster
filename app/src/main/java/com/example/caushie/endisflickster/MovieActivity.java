2acc4676b5bdfc3e2203f598c0f19211ee5a1626
Endi <mrendi29@gmail.com>
2019-01-15 01:09:24 -0500

    Second Commit Added RecyclerView

--- app/src/main/java/com/example/caushie/endisflickster/MovieActivity.java
+++ app/src/main/java/com/example/caushie/endisflickster/MovieActivity.java
@@ -1,9 +1,12 @@
 package com.example.caushie.endisflickster;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.example.caushie.endisflickster.adapter.MoviesAdapter;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -11,6 +14,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
@@ -20,11 +24,31 @@ public class MovieActivity extends AppCompatActivity {
 
     private static  String MOVIE_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed ";
     // We will create the list of movies (data model) and we will use it to display the movies into the screen.
+
     List<Movie> movies;
+    //Key steps using a Recycler View.
+    //Add RecyclerView support library to the gradle build file Done
+    //Define a model class to use as the data source -DOne
+    //Add a RecyclerView to your activity to display the items -Done
+    //Create a custom row layout XML file to visualize the item -Done
+    //Create a RecyclerView.Adapter and ViewHolder to render the item -Done
+    //Bind the adapter to the data source to populate the RecyclerView
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        //Bind the adapter to the data source to populate the RecyclerView
+        RecyclerView rvMovies = findViewById(R.id.rvMovies);
+        //We have not declared movies as a new arraylist  so we dont crash (because it was null)
+        movies = new ArrayList<>();
+        //Create an instance of the movies adapter that we just wrote.
+        final MoviesAdapter adapter = new MoviesAdapter(this, movies);
+        //Add a layout manager into the recycle view so the recycle view knows how to put the items on the screen.
+        rvMovies.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        //Set the adaptor on the recycle view.
+        rvMovies.setAdapter(adapter);
 
         AsyncHttpClient client = new AsyncHttpClient();
         //We are making a network request on that url
@@ -45,8 +69,11 @@ public class MovieActivity extends AppCompatActivity {
                     //1. get the JSon array from the database.
                     JSONArray movieJsonArray = response.getJSONArray("results");
 
-                     movies = Movie.fromJsonArray(movieJsonArray);
+                    //   movies = Movie.fromJsonArray(movieJsonArray);
 
+                    movies.addAll(Movie.fromJsonArray(movieJsonArray));
+
+                    adapter.notifyDataSetChanged();
                     Log.d("success",movies.toString());
                 } catch (JSONException e) {
                     e.printStackTrace();
---
+++ app/src/main/java/com/example/caushie/endisflickster/adapter/MoviesAdapter.java
@@ -0,0 +1,84 @@
+package com.example.caushie.endisflickster.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.caushie.endisflickster.R;
+
+import java.util.List;
+
+import model.Movie;
+
+//after finishing work assigning the instances of variables we should extend the adapter of the recycler view with the one we created below.
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder> {
+
+    Context context;
+    List<Movie> movies;
+
+    public MoviesAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("Smile", "onCreateViewHolder");
+        //onCreateViewHolder is responsible for inflating the  xml we created and the result is a view.
+        View view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+
+        //returns a new view  holder with that particular view by calling the constructor.
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("Smile", "onBindViewHolder: " + position);
+
+        //take the data that lives in that positions and attach that data to a particular viewholder that the adapter has given to us.
+        Movie movie = movies.get(position);
+        //Bind the movie data into the viewholder.
+        holder.bind(movie);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    //First thing define viewholder.
+    //Then exetend the RecycleView Viewholder.
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+
+        }
+    }
+
+}
